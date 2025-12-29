925488803be0d523e5f7c6b5fb676c6f1cccfdf4
Ryo-Shigematsu <sgmtryo1211@gmail.com>
2025-12-29 16:07:52 +0900

    feat: ESLint設定を更新し、TypeScriptプラグインを適用

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,51 @@
-import eslintPluginTs from "@typescript-eslint/eslint-plugin";
-import eslintParserTs from "@typescript-eslint/parser";
+import js from "@eslint/js";
+import typescript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
 
 export default [
+  js.configs.recommended,
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "*.config.js",
+      "*.config.ts",
+    ],
+  },
   {
     files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
-      parser: eslintParserTs,
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures:  {
+          jsx: true,
+        },
+      },
+      globals: {
+        React: "readonly",
+        JSX: "readonly",
+        HTMLElement: "readonly",
+        HTMLDivElement: "readonly",
+        HTMLButtonElement: "readonly",
+        HTMLInputElement: "readonly",
+        HTMLTableElement: "readonly",
+        HTMLTableSectionElement: "readonly",
+        HTMLTableRowElement: "readonly",
+        HTMLTableCellElement: "readonly",
+        HTMLTableCaptionElement: "readonly",
+      },
     },
-    plugins: {
-      "@typescript-eslint": eslintPluginTs,
+    plugins:  {
+      "@typescript-eslint": typescript,
     },
-    rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
+    rules:  {
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ];
