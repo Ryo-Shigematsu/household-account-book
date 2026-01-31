6e89a3789c76ce77742051c0becdaa062c2c96c4
Ryo-Shigematsu <sgmtryo1211@gmail.com>
2026-01-31 20:54:43 +0900

    ESLint対応

--- src/__tests__/sample.test.ts
+++ src/__tests__/sample.test.ts
@@ -1,9 +1,9 @@
-describe('Sample Test', () => {
-  it('基本的な計算が正しく動作する', () => {
+describe("Sample Test", () => {
+  it("基本的な計算が正しく動作する", () => {
     expect(1 + 1).toBe(2);
   });
 
-  it('文字列の結合が正しく動作する', () => {
-    expect('Hello' + ' ' + 'World').toBe('Hello World');
+  it("文字列の結合が正しく動作する", () => {
+    expect("Hello" + " " + "World").toBe("Hello World");
   });
 });
